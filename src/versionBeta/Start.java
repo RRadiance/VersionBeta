1f8c9339de68360e758cf7519aabe6a78ecefb5a
Matthew Huynh <mattrhuynh@gmail.com>
2018-12-21 14:48:11 -0500

    Added start file

---
+++ src/versionBeta/Start.java
@@ -0,0 +1,22 @@
+//package versionBeta;
+//
+//import javafx.application.Application;
+//import javafx.stage.Stage;
+//
+//public class Start extends Application {
+//	
+//	Model model; // Model
+//	View view; // View + Controller
+//	
+//	public static void main(String[]args) {
+//		launch(args);
+//	}
+//	
+//	@Override
+//	public void start(Stage stage) throws Exception {
+//		this.model = new Model();
+//
+//		// View + Controller
+//		this.view = new View(model, stage);
+//	}
+//}
